e9cf1981bf2670e008a4e4eb311e66052bd20edc
Hardik <hardik.goswami@influx.co.in>
2024-02-05 12:25:39 +0530

    test : create chandrayaan initialize

---
+++ src/test/java/GalacticSpaceCraftControllerTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+
+public class GalacticSpaceCraftControllerTest {
+
+    @Test
+    public void testExecuteCommands(){
+        Chandrayaan spacecraft = new Chandrayaan(0,0,0,Direction.N);
+    }
+}
